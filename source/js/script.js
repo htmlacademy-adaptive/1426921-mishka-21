0f85848d3f4bd909f380f9f63af42ef51a16d6fb
Olga Tsyganova <superolk@mail.ru>
2021-01-12 20:08:03 +0300

    new

--- source/js/script.js
+++ source/js/script.js
@@ -2,14 +2,14 @@ const searchButton = document.querySelector(".featured__button");
 const cart = document.querySelectorAll(".cart");
 const popup = document.querySelector(".modal-cart");
 
-searchButton.addEventListener ("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.toggle("modal-show");
-});
+searchButton ? searchButton.addEventListener ("click", function (evt) {
+evt.preventDefault();
+popup.classList.toggle("modal-show");
+}): '';
 
 for(var i = 0; i < cart.length; i++) {
-    cart[i].addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.toggle("modal__show");
-    });
-    }
+cart[i].addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.toggle("modal-show");
+});
+}
